6d7721c0b744d0a3fd7bc731cc00fe40ebb591ff
David Sims <davidesims@icloud.com>
2017-07-27 22:01:41 -0700

    contact route

--- server/utils/contact.js
+++ server/utils/contact.js
@@ -1,22 +1,22 @@
 /* @flow */
-import sendmail from 'sendmail';
+const sendmail = require('sendmail')();
 
-const sendMail = (req, res) => {
+const contact = (req, res) => {
   const html = `
     Name: ${req.body.name}
     Email: ${req.body.email}
-    Phone: ${req.body.phone}
+    Phone: ${req.body.phoneNumber}
     Company: ${req.body.company}
     Preferred Method of Contact: ${req.body.contactMethod}
     Message: ${req.body.msg}
   `;
 
   sendmail({
-    from: req.body.email,
+    from: 'itsupport@pacificexpeditors.com',
     to: 'itsupport@pacificexpeditors.com',
-    subject: 'Email from ' + req.body.email,
-    html,
-  }, (err, reply) => {
+    subject: `Email from ${req.body.name}`,
+    html
+  }, function(err, reply) {
     if (err) {
       return res.send({
         statusCode: 400,
@@ -31,4 +31,4 @@ const sendMail = (req, res) => {
   });
 };
 
-module.exports = sendMail;
+module.exports = contact;
