0997600a324f956d6a4cda34f6d7ee1a6c29cfbb
David Sims <davidesims@icloud.com>
2017-07-28 18:23:00 -0700

    updated contact route

--- server/utils/contact.js
+++ server/utils/contact.js
@@ -1,22 +1,31 @@
 /* @flow */
-const ses = require('node-ses')({
-  key: '',
-  secret: ''
+import { createClient } from 'node-ses';
+
+const client = createClient({
+  key: process.env.AWS_KEY,
+  secret: process.env.AWS_SECRET
 });
 
 const contact = (req, res) => {
+  if (!req.body.name || !req.body.email || !req.body.phoneNumber) {
+    return res.send({
+      statusCode: 400,
+      msg: "Missing one or more fields."
+    });
+  }
+  
   const message = `
-    Name: ${req.body.name}
-    Email: ${req.body.email}
-    Phone: ${req.body.phoneNumber}
-    Company: ${req.body.company}
-    Preferred Method of Contact: ${req.body.contactMethod}
-    Message: ${req.body.msg}
+    Name: ${req.body.name}<br />
+    Email: ${req.body.email}<br />
+    Phone: ${req.body.phoneNumber}<br />
+    Company: ${req.body.company}<br />
+    Preferred Method of Contact: ${req.body.contactMethod}<br />
+    Message: <p>${req.body.msg}</p>
   `;
 
-  ses({
+  client.sendEmail({
     from: 'itsupport@pacificexpeditors.com',
-    to: 'itsupport@pacificexpeditors.com',
+    to: 'solutions@pacificexpeditors.com',
     subject: `Email from ${req.body.name}`,
     message
   }, function(err, data, info) {
