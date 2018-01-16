5eca3365116e0006e2e4254a3c8bce8825cd0a86
David Sims <davidsims15@gmail.com>
2018-01-15 16:42:28 -0800

    added restart script for heroku

---
+++ server/utils/restart.js
@@ -0,0 +1,14 @@
+const Heroku = require('heroku.node');
+
+const client = new Heroku({
+  email: process.env.HEROKU_EMAIL,
+  api_key: process.env.HEROKU_API_KEY
+});
+
+const restart = (req, res) => {
+  client.app('pacificexpeditors').dynos.restart((err, msg) => {
+    res.send(err || msg);
+  });
+}
+
+module.exports = restart;
